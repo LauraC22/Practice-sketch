968795e188550244048d6c5b0dfaf3438766ce61
LauraC22 <lauraccervellini@lauras-mbp-6.home>
2020-10-30 09:53:24 -0400

    Initial setup

---
+++ sketch.js
@@ -0,0 +1,21 @@
+let elem;
+
+function setup() {
+  // put setup code here
+  createCanvas(windowWidth, windowHeight)
+  background(0,220,220)
+  elem= createElement('h2', 'I AM A CIRCLE')
+  elem.position(150, 170)
+  elem.style('color', 'yellow')
+
+}
+
+function draw() {
+  // put drawing code here
+  
+  fill("purple")
+  noStroke()
+  circle(windowWidth/2,windowHeight/2,400)
+ 
+
+}
